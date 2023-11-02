5c848e5b5885e40dfd00f6edda6c853c2bce39a5
danvalle00 <danieldovalle10@gmail.com>
2023-11-02 01:32:25 -0300

    more challenges

---
+++ C# Player's Guide/TheMagicCannon/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Title = "The Magic Cannon";
+
+for (int turn = 1; turn < 101;  turn++)
+{
+    Console.Write($"{turn}: ");
+    if(turn % 3 == 0 && turn % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Eletric and Fire!");
+        Console.ForegroundColor = ConsoleColor.White;
+        continue;
+    }
+    else if (turn % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Fire");
+        Console.ForegroundColor = ConsoleColor.White;
+        continue;
+    }
+    else if (turn % 5 == 0)
+    {
+        Console.ForegroundColor= ConsoleColor.Yellow;
+        Console.WriteLine("Eletric");
+        Console.ForegroundColor = ConsoleColor.White;
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Normal");
+    }
+}
---
+++ C# Player's Guide/ThePrototype/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Title = "The Prototype";
+
+int numberToGuess;
+do
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    numberToGuess = Convert.ToInt32(Console.ReadLine());
+}
+while (numberToGuess < 0 || numberToGuess > 100);
+
+Console.Clear();
+
+Console.Write("User 2, guess the number: ");
+int guessedNumber = Convert.ToInt32(Console.ReadLine());
+do
+{
+    if (guessedNumber > numberToGuess)
+    {
+        Console.WriteLine($"{guessedNumber} is to high.");
+    }
+    else if (guessedNumber < numberToGuess)
+    {
+        Console.WriteLine($"{guessedNumber} is to low.");
+    }
+    else 
+    {
+        Console.WriteLine("You guessed the number!");
+        break;
+    }
+    Console.Write("What's your next guess? ");
+    guessedNumber = Convert.ToInt32(Console.ReadLine());
+}
+while (guessedNumber != numberToGuess);
