ed4a72a029ac63974aa0f9bc939dd294b449f143
danvalle00 <danieldovalle10@gmail.com>
2023-11-03 06:39:01 -0300

    enum and tuples with SOUPPP

---
+++ C# Player's Guide/Simula'sSoup/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.Title = "Simula's Soup";
+
+Console.WriteLine("Choose the elements of the soup for Simula to make it!");
+FoodTypes desiredType = PickFoodType();
+FoodMainIngredient desiredIngredient = PickFoodMainIngredient();
+FoodSeasoning desiredSeasoning = PickFoodSeasoning();
+
+var desiredFoodCombination = (Type: desiredType, MainIngredient: desiredIngredient, Seasoning: desiredSeasoning);
+Console.WriteLine($"Your desired food is a {desiredFoodCombination.Type} with {desiredFoodCombination.MainIngredient} as a main component and a {desiredFoodCombination.Seasoning} seasoning.");
+Console.WriteLine("Hope you enjoy it :)");
+
+FoodTypes PickFoodType()
+{
+    Console.WriteLine("Which variation of food do you like? ");
+    Console.WriteLine($"1 -> {FoodTypes.Gumbo}");
+    Console.WriteLine($"2 -> {FoodTypes.Soup}");
+    Console.WriteLine($"3 -> {FoodTypes.Stew}");
+    int numberChoice = Convert.ToInt32(Console.ReadLine());
+
+    FoodTypes typeChoice = numberChoice switch
+    {
+        1 => typeChoice = FoodTypes.Gumbo,
+        2 => typeChoice = FoodTypes.Soup,
+        3 => typeChoice = FoodTypes.Stew,
+    };
+    return typeChoice;
+}
+
+FoodMainIngredient PickFoodMainIngredient()
+{
+    Console.WriteLine("Which Main Ingredient do you like? ");
+    Console.WriteLine($"1 -> {FoodMainIngredient.Mushroom}");
+    Console.WriteLine($"2 -> {FoodMainIngredient.Chicken}");
+    Console.WriteLine($"3 -> {FoodMainIngredient.Carrot}");
+    Console.WriteLine($"4 -> {FoodMainIngredient.Potato}");
+    int numberChoice = Convert.ToInt32(Console.ReadLine());
+
+    FoodMainIngredient ingredientChoice = numberChoice switch
+    {
+        1 => ingredientChoice = FoodMainIngredient.Mushroom,
+        2 => ingredientChoice = FoodMainIngredient.Chicken,
+        3 => ingredientChoice = FoodMainIngredient.Carrot,
+        4 => ingredientChoice = FoodMainIngredient.Potato,
+    };
+    return ingredientChoice;
+}
+
+FoodSeasoning PickFoodSeasoning()
+{
+    Console.WriteLine("Which Seasoning do you like? ");
+    Console.WriteLine($"1 -> {FoodSeasoning.Spicy}");
+    Console.WriteLine($"2 -> {FoodSeasoning.Salty}");
+    Console.WriteLine($"3 -> {FoodSeasoning.Sweet}");
+    int numberChoice = Convert.ToInt32(Console.ReadLine());
+
+    FoodSeasoning seasoningChoice = numberChoice switch
+    {
+        1 => seasoningChoice = FoodSeasoning.Spicy,
+        2 => seasoningChoice = FoodSeasoning.Salty,
+        3 => seasoningChoice = FoodSeasoning.Sweet,
+    };
+    return seasoningChoice;
+        
+}
+enum FoodTypes { Soup, Stew, Gumbo};
+enum FoodMainIngredient {Mushroom, Chicken, Carrot, Potato};
+enum FoodSeasoning {Spicy, Salty, Sweet};
+
---
+++ C# Player's Guide/Simula'sTest/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Title = "Simula's Test";
+
+ChestState chestState = ChestState.Locked; 
+
+while(true)
+{
+    Console.Write($"The chest is {chestState}. What do you want to do? ");
+    string chestAction = Console.ReadLine();
+
+    // Possible refactoring is changing to switch case.
+    if (chestAction == "unlock" && chestState == ChestState.Locked)
+    {
+        chestState = ChestState.Closed;
+        continue;
+    }
+    else if (chestAction == "open" && chestState == ChestState.Closed)
+    {
+        chestState = ChestState.Open;
+        continue;
+    }
+    else if (chestAction == "close" && chestState == ChestState.Open)
+    {
+        chestState = ChestState.Closed;
+        continue;
+    }
+    else if (chestAction == "lock" && chestState == ChestState.Closed)
+    {
+        chestState = ChestState.Locked;
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Can't do this action, try another thing.");
+        continue;
+    }
+};
+
+enum ChestState {Open, Closed, Locked};
