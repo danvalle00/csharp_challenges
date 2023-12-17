3f7589de1f3b9862ff3e1d5d4696e0af692b6b51
danvalle00 <danieldovalle10@gmail.com>
2023-12-17 15:57:58 -0300

    generics challenge and fountain

---
+++ C# Player's Guide/OOP/BossBattles/TheFountainOfObjects/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.Title = "The Fountain of Objects";
+
---
+++ C# Player's Guide/OOP/ColoredItems/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Title = "Colored Items";
+
+ColoredItem<Sword> blueSword = new ColoredItem<Sword>(ConsoleColor.Blue, new Sword());
+ColoredItem<Axe> greenAxe = new ColoredItem<Axe>(ConsoleColor.Green, new Axe());
+ColoredItem<Bow> redBow = new ColoredItem<Bow>(ConsoleColor.Red, new Bow());
+
+blueSword.Display();
+greenAxe.Display();
+redBow.Display();
+
+public class ColoredItem<T>
+{
+    public ConsoleColor Color { get; }
+    public T ItemType { get; }
+
+    public ColoredItem(ConsoleColor color, T item)
+    {
+        Color = color;
+        ItemType = item;
+    }
+    
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(ItemType);
+    }
+}
+public class Sword
+{
+
+}
+public class Bow
+{
+
+}
+
+public class Axe
+{
+
+}
+
+
