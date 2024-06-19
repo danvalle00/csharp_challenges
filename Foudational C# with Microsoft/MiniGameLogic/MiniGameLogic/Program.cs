29278e8df638220fa623dc5fc77f804affd2462a
danvalle00 <danieldovalle10@gmail.com>
2024-06-19 17:16:04 -0300

    zzzz

--- Foudational C# with Microsoft/MiniGameLogic/MiniGameLogic/Program.cs
+++ Foudational C# with Microsoft/MiniGameLogic/MiniGameLogic/Program.cs
@@ -6,26 +6,41 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
+
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("Doubles!! +2 Points");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("Triples!!! +6 Points");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("Doubles!! +2 Points");
+        total += 2;
+    }
+
+    Console.WriteLine($"Dice roll with the bonus: {roll1} + {roll2} + {roll3} = {total}");
 }
 
-if ((roll1 == roll2) && (roll2 == roll3))
+
+if (total >= 16)
 {
-    Console.WriteLine("Triples!!! +6 Points");
-    total += 6;
+    Console.WriteLine("You win a new Car");
 }
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You Win!");
+    Console.WriteLine("You win a Laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two");
+}
+else
+{
+    Console.WriteLine("You Lose! ;p");
 }
 
-if (total < 15)
-{
-    Console.WriteLine("You Lose");
-}
 
