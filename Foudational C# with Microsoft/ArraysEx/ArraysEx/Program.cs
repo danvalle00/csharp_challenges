e7a0707f5aa95c59eb46716c718b65f00d4e7227
danvalle00 <danieldovalle10@gmail.com>
2024-06-20 20:21:42 -0300

    arrays and starting guided

---
+++ Foudational C# with Microsoft/ArraysEx/ArraysEx/Program.cs
@@ -0,0 +1,22 @@
+﻿/* int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (var item in inventory)
+{
+    sum += item;
+    bin++;
+    Console.WriteLine($"Bin {bin}: {item} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory"); */
+
+string[] itemsId = { "B123", "C234", "A345", "C15", "B177", "G3003", "G235", "B179" };
+
+foreach (string fraudulentItem in itemsId)
+{
+    if (fraudulentItem.StartsWith("B"))
+    {
+        Console.WriteLine(fraudulentItem);
+    }
+}
