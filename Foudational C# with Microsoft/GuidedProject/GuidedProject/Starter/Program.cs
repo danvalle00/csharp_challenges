30ebf7f216c3f4866d63c9774dd6a9cabb305b25
danvalle00 <danieldovalle10@gmail.com>
2024-06-20 21:09:36 -0300

    added switchcases

--- Foudational C# with Microsoft/GuidedProject/GuidedProject/Starter/Program.cs
+++ Foudational C# with Microsoft/GuidedProject/GuidedProject/Starter/Program.cs
@@ -7,17 +7,45 @@ int[] andrewScores = { 92, 89, 81, 96, 90 };
 int[] emmaScores = { 90, 85, 87, 98, 68 };
 int[] loganScores = { 90, 95, 87, 88, 96 };
 
+string[] studentsNames = { "Sophia", "Andrew", "Emma", "Logan" };
 
 int sophiaSum = 0;
 int andrewSum = 0;
 int emmaSum = 0;
 int loganSum = 0;
 
-foreach (int score in sophiaScores)
-{
-    sophiaSum += score;
-}
 
+foreach (string studentName in studentsNames)
+{
+    switch (studentName)
+    {
+        case "Sophia":
+            foreach (int score in sophiaScores)
+            {
+                sophiaSum += score;
+            }
+            break;
+        case "Andrew":
+            foreach (int score in andrewScores)
+            {
+                andrewSum += score;
+            }
+            break;
+        case "Emma":
+            foreach (int score in emmaScores)
+            {
+                emmaSum += score;
+            }
+            break;
+        case "Logan":
+            foreach (int score in loganScores)
+            {
+                loganSum += score;
+            }
+            break;
+    }
+
+}
 
 decimal sophiaScore;
 decimal andrewScore;
