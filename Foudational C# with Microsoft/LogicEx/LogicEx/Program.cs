b1adf0201781245ee11baa19763b0405a0dd022a
danvalle00 <danieldovalle10@gmail.com>
2024-06-22 22:47:04 -0300

    logic example

---
+++ Foudational C# with Microsoft/LogicEx/LogicEx/Program.cs
@@ -0,0 +1,20 @@
+﻿string permission = "Admin|Manager";
+int level = 56;
+
+
+if (permission.Contains("Admin") && level > 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin") && level <= 55)
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager") && level >= 20) // need to change the permission zzz
+{
+    Console.WriteLine("Contact Admin for acess");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges");
+}
